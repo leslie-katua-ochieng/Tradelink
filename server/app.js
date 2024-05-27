3bec1f59166ac2bd83cc3cfc3d64f87a3ffa0b28
Leslie Katua <katualeslie@gmail.com>
2024-05-27 16:25:31 +0300

    Update: Connecting Express to MongoDB

--- server/app.js
+++ server/app.js
@@ -1,11 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+const express = require('express');
+const bodyParser = require('body-parser');
+const db = require('./database/db');
+const app = express();
+const cors=require('cors');
+const corsOptions={
+  origin:'*',
+  credentials:true,
+  optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+app.use('/', routes);
+app.listen(5000, () => {
+    console.log('Server started on port 5000');
 })
